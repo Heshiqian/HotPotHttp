b2b8e926ec5c45c94d9ec2bbe288c6df77776fd6
Heshiqian <heshiqian3@126.com>
2019-09-05 18:51:43 +0800

    [master] 运行环境搭建

---
+++ runtime/src/MyStarter.java
@@ -0,0 +1,7 @@
+import cn.heshiqian.hotpothttp.core.HotPotHttpApplicationStarter;
+
+public class MyStarter extends HotPotHttpApplicationStarter {
+    public static void main(String[] args) {
+        run(args,MyStarter.class);
+    }
+}
